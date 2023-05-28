7f55b907aa5f1498ce596db1baf67dc017acf67b
amithvinod <amithvinod8@gmail.com>
2023-05-28 11:03:49 +0530

    second

--- bot.py
+++ bot.py
@@ -30,8 +30,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 TOKEN = '6092431726:AAEwZY_56XxdHs35uaLq0c0xAqVvYjX5Sto'
 
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
+
 def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Search any youtube video')
@@ -96,35 +95,26 @@ def error(update, context):
 
 def main():
     """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
+   
     updater = Updater(TOKEN, use_context=True)
     
-    # Get the dispatcher to register handlers
+   
     dp = updater.dispatcher
     
-    # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
     
-    # on noncommand i.e message - echo the message on Telegram
     
     dp.add_handler(MessageHandler(Filters.text,video))
     
     dp.add_handler(InlineQueryHandler(hi))
-    # log all errors
     dp.add_error_handler(error)
     
-    # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
     updater.bot.setWebhook('https://whispering-thicket-23836.herokuapp.com/' + TOKEN)
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
     
 
